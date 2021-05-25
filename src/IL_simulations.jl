0d7a56eb005b7e9a96b979791a3a634dc4b2d2ad
viniciuspiccoli <viniciuspiccoli2008@hotmail.com>
2021-05-25 14:18:19 -0300

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/IL_simulations.jl
@@ -0,0 +1,5 @@
+module IL_simulations
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using IL_simulations
+using Test
+
+@testset "IL_simulations.jl" begin
+    # Write your tests here.
+end
