9035c44309841ec3913892d83ae3c855f46d3332
viniciuspiccoli <viniciuspiccoli2008@hotmail.com>
2021-06-14 12:15:51 -0300

    first top function

--- src/test.jl
+++ src/test.jl
@@ -13,12 +13,8 @@ c       =  2.50
 data = IL_simulations.Data_il(protein="$input_dir/ubq.pdb", MMP = MMP, cation = cation1, anion = anion1,MM = MM1,c=c)
 nions, nwater, sides = number_of_ions(data)
 
-input_dir = "/home/viniciusp/Documents/doutorado/ANALYSE/Repository/IL_simulations.jl/ubiquitin_files/topol.top"
 
-
-
-
-#Topology(dict, top_dir, input_dir, data, nions, nwater)
+IL_simulations.top(dict, top_dir, input_dir, data, nions, nwater)
 
 
 
--- src/topology.jl
+++ src/topology.jl
@@ -1,66 +1,41 @@
-#export Topology
+#export top
 
-function topology(file,dict, data)
-         f = open(file,"r")
-         for line in eachline(f)
-           println(line)
-         end
-       end
-
-
-
-function top(dict,top_dir, prot_file, data, nions, nwat)
+function top(dict,top_dir, prot_dir, data, nions, nwater)
   
-#  anion  = data.anion
-#  cation = data.cation
+  anion  = data.anion
+  cation = data.cation
 
-#  file = open("$prot_dir/topol.top","r")  
-  file = open(prot_file,"r")
-  nlines = countlines(file)# number of lines of the original topol.top
+  file = open("$prot_dir/topol.top","r")  
+#  nlines = countlines(f)# number of lines of the original topol.top
 
- # println(nlines)
- # println(file)
+  topol  = open("./topol.top","w") # creation of the topol.top which will be used in the simulations
 
+  n = 0
   for line in eachline(file)
-    println(line)
-    println("está dentro do loop")
+     n = n + 1
+     if n==19
+       println(topol,"#define _FF_OPLS")
+       println(topol,"#define _FF_OPLSAA")
+       println(topol,"                 ")
+     elseif n==22
+       println(topol,"      ")
+       println(topol,"; LOAD Atom types and Molecules")
+       println(topol,"#include \"$top_dir/$(data.cat_atomtypes)\" ")
+       println(topol,"#include \"$top_dir/$(data.cat_par)\" ")
+       println(topol,"#include \"$top_dir/$(data.an_atomtypes)\" ")
+       println(topol,"#include \"$top_dir/$(data.an_par)\" ")
+       println(topol,"             ")
+     elseif n>19 && n != 22 && occursin("SOL",line)==false
+       println(topol, line)
+     elseif occursin("SOL",line)==true
+       println(topol,line)
+       println(topol,"SOL                $nwater")
+       println(topol,"$(dict[anion])                $nions")	
+       println(topol,"$(dict[cation])                $nions")
+     end
   end
-  
-  println("passou do loop")
 
-
-
-#  topol  = open("./topol.top","w") # creation of the topol.top which will be used in the simulations
-
-#  println("vai iniciar o loop")
-#
-#  n = 0
-#  for line in eachline(file)
-#     println("entrou no loop")
-#     n = n + 1
-#     if n==19
-#       println("passou da linha 19")
-#       println(topol,"#define _FF_OPLS")
-#       println(topol,"#define _FF_OPLSAA")
-#     elseif n==26
-#       println("   ")
-#       println("passou da linha 26")
-#       println(topol,"$top_dir/$(data.cat_atomtypes)")
-#       println(topol,"$top_dir/$(data.cat_par)")
-#       println(topol,"$top_dir/$(data.an_atomtypes)")
-#       println(topol,"$top_dir/$(data.an_par)")
-#     elseif n>19 && n != 26 && occursin("SOL",line)==false
-#       println(topol, line)
-#       println("hehe")
-#     elseif occursin("SOL",line)==true
-#       println(topol,line)
-#       println(topol,"SOL                $nwater")
-#       println(topol,"$(dict[anion])                $nions")	
-#       println(topol,"$(dict[cation])                $nions")
-#     end
-#  end
-#
-#  close(file)
-#  close(topol)
+  close(file)
+  close(topol)
  
 end
