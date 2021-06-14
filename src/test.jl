0da5888d755f1fa851713678e442e14d9d899e83
viniciuspiccoli <viniciuspiccoli2008@hotmail.com>
2021-06-14 12:39:43 -0300

    added files

--- src/test.jl
+++ src/test.jl
@@ -2,6 +2,7 @@ using IL_simulations
 
 
 top_dir   = "/home/viniciusp/Documents/doutorado/ANALYSE/Repository/IL_simulations.jl/VSIL/ITP"
+pdb_dir   = "/home/viniciusp/Documents/doutorado/ANALYSE/Repository/IL_simulations.jl/VSIL/PDB"
 input_dir = "/home/viniciusp/Documents/doutorado/ANALYSE/Repository/IL_simulations.jl/ubiquitin_files"
 
 MM1     =  201.06
@@ -12,9 +13,8 @@ c       =  2.50
 
 data = IL_simulations.Data_il(protein="$input_dir/ubq.pdb", MMP = MMP, cation = cation1, anion = anion1,MM = MM1,c=c)
 nions, nwater, sides = number_of_ions(data)
-
-
 IL_simulations.top(dict, top_dir, input_dir, data, nions, nwater)
+pack_input(data,pdb_dir,nions,nwater,sides)
 
 
 
